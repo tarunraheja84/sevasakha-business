35a8e708ae25263d769d5ca36197dd6aebd06c06
Tarun <tarunraheja84@gmail.com>
2025-04-13 21:54:11 +0530

    vercel limit changed

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,11 @@ const nextConfig = {
     images: {
         domains: ['tarunraheja84.s3.ap-south-1.amazonaws.com'],
       },
+      experimental: {
+        serverActions: {
+          bodySizeLimit: '10mb',
+        },
+      },
 };
 
 export default nextConfig;
