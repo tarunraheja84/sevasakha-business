245817584effdd526bca767bb8b98e78ff2a97dc
Tarun <tarunraheja84@gmail.com>
2025-04-13 21:58:22 +0530

    next commit

--- next.config.mjs
+++ next.config.mjs
@@ -2,12 +2,7 @@
 const nextConfig = {
     images: {
         domains: ['tarunraheja84.s3.ap-south-1.amazonaws.com'],
-      },
-      experimental: {
-        serverActions: {
-          bodySizeLimit: '10mb',
-        },
-      },
+      }
 };
 
 export default nextConfig;
