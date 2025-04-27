195875a3f4758d3f502f7a56c1738adb1d8fc39d
Tarun <tarunraheja84@gmail.com>
2025-04-27 21:07:15 +0530

    next commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: ['tarunraheja84.s3.ap-south-1.amazonaws.com'],
-      }
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https', // The protocol for the image URL
+        hostname: 'tarunraheja84.s3.ap-south-1.amazonaws.com', // The hostname of the remote image source
+        pathname: '/**', // The path pattern to match images
+      },
+    ],
+  },
 };
 
 export default nextConfig;
